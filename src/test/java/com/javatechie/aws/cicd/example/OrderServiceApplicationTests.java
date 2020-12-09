5119a22d51a5dd16d67f0c16932c19e196f24d6e
bhuvaneshwarrao <39791824+bhuvaneshwarrao@users.noreply.github.com>
2020-12-09 21:38:27 +0530

    Update OrderServiceApplicationTests.java

--- src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
+++ src/test/java/com/javatechie/aws/cicd/example/OrderServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.javatechie.aws.cicd.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest  
 class OrderServiceApplicationTests {
 
 	@Test
